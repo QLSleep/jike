6826be65aa217b6aa52a77499fe2e35e47a5860e
QLSleep <qzkabcd@qq.com>
2026-01-10 00:51:24 +0800

    获取token代码编写

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import { Button } from 'antd'
-import { RouterProvider } from 'react-router-dom'
-import router from './router'
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => {
   return (
-    <RouterProvider router={router}/>
-  )
-}
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
+};
 
-export default App
+export default App;
